d30e9366250706c9d32e2d45e6a0942d84dcc322
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-01-22 17:19:19 +0100

    using reactivity statements

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,21 @@ let name = 'world';
 let src = 'favicon.png';
 let string = `This string contains some <strong>HTML!!!</strong>`;
 let count = 0;
+let ccc = 0;
 $: doubled = count * 2;
+$: if (count >= 10) {
+  alert(`count is dangerously high!`);
+  count = 9;
+}
+$: console.log(`the count is ${count}`);
+$: console.log(`the ccc is ${ccc}`);
+$: console.log(`both are ${count} and ${ccc}`);
 
 function handleClick() {
   count += 1;
+}
+function handleCcc() {
+  ccc += 1;
 }
 </script>
 
@@ -19,6 +30,9 @@ function handleClick() {
 <p>{@html string}</p>
 <button on:click={handleClick}>
   Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+<button on:click={handleCcc}>
+  Clicked {ccc} {count === 1 ? 'time' : 'times'}
 </button>
 <p>{count} doubled is {doubled}</p>
 
