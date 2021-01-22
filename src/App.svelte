096d9d58f45f50c36e39d54e6fc2ad1e3cf40aa2
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-01-22 17:08:17 +0100

    using an example with html tags passed by parameter

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,14 @@ import Nested from './components/Nested.svelte';
 
 let name = 'world';
 let src = 'favicon.png';
+let string = `This string contains some <strong>HTML!!!</strong>`;
 </script>
 
 <h1>Hello {name}!</h1>
 <img src={src} alt="A man dances" />
 <p class="custom-1">This is a paragraph.</p>
 <Nested />
+<p>{@html string}</p>
 
 <style>
 p.custom-1 {
